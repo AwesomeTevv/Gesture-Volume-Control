d195a8bd21ddb2e4ca3bcd16df125719ca0bf8e3
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-28 20:07:15 +0200

    Added cam size and fps display

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,10 +2,25 @@ import cv2
 import time
 import numpy as np
 
+cam_width = 1280
+cam_height = 720
+
 cap = cv2.VideoCapture(0)
+
+cap.set(3, cam_width)
+cap.set(4, cam_height)
+
+current_time = 0
+previous_time = 0
 
 while True:
     success, img = cap.read()
+    
+    current_time = time.time()
+    fps = 1 / (current_time - previous_time)
+    previous_time = current_time
+    
+    cv2.putText(img, f'fps: {int(fps)}', (50, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
     
     cv2.imshow("Image", img)
     cv2.waitKey(1)
