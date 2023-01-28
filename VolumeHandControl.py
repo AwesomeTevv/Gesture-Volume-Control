b5fd40b81035418440f1e70cd15aba393c8e5d6c
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-29 00:45:19 +0200

    Drew a line between the thumb and index

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -14,8 +14,22 @@ cap.set(4, cam_height)
 current_time = 0
 previous_time = 0
 
+detector = htm.handDetector(detectionConfidence = 0.7)
+
 while True:
     success, img = cap.read()
+    
+    img = detector.findHands(img)
+    
+    landmark_list = detector.findPosition(img, draw = False)
+    if len(landmark_list) != 0:
+        x1, y1 = landmark_list[4][1], landmark_list[4][2]
+        x2, y2 = landmark_list[8][1], landmark_list[8][2]
+        
+        cv2.circle(img, (x1, y1), 10, (255, 255, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 255, 0), cv2.FILLED)
+        
+        cv2.line(img, (x1, y1), (x2, y2), (255, 255, 0), 3)
     
     current_time = time.time()
     fps = 1 / (current_time - previous_time)
